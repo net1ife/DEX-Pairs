bcbec17353cdf76a19c33774bcfd5de20b423fa8
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-16 16:58:20 -0500

    Update pairs.py

--- pairs.py
+++ pairs.py
@@ -2,31 +2,36 @@ import streamlit as st
 import requests
 
 
+EXCHANGE_URLS = {
+    'uniswap': 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+    'sushiswap': 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
+}
+
+
 def get_token_pairs_with_highest_liquidity(exchange_name, num_pairs):
     # Set the exchange subgraph URL
-    if exchange_name.lower() == 'uniswap':
-        subgraph_url = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
-    elif exchange_name.lower() == 'sushiswap':
-        subgraph_url = 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange'
+    subgraph_url = EXCHANGE_URLS[exchange_name.lower()]
 
     # Query the subgraph to get token pairs sorted by liquidity
-    query = '''
-        query {
-            pairs(first: %d, orderBy: reserveUSD, orderDirection: desc) {
-                token0 {
+    query = f'''
+        query {{
+            pairs(first: {num_pairs}, orderBy: reserveUSD, orderDirection: desc) {{
+                token0 {{
                     symbol
                     id
-                }
-                token1 {
+                }}
+                token1 {{
                     symbol
                     id
-                }
+                }}
                 reserveUSD
-            }
-        }
-    ''' % num_pairs
+            }}
+        }}
+    '''
 
     response = requests.post(subgraph_url, json={'query': query})
+    response.raise_for_status()
+
     data = response.json()
 
     if 'data' in data:
@@ -45,23 +50,27 @@ def format_pair(pair):
     return f"Pair: {token0_symbol}/{token1_symbol}, Address: {token0_address}/{token1_address}, Liquidity: ${reserve_usd}"
 
 
-# Streamlit UI
-st.title('Token Pairs with Highest Liquidity')
+def main():
+    # Streamlit UI
+    st.title('Token Pairs with Highest Liquidity')
 
-exchange_name = st.sidebar.selectbox('Select Exchange', ['Uniswap', 'Sushiswap'])
-num_pairs = st.sidebar.number_input('Number of Pairs', min_value=1, max_value=20, value=10)
+    exchange_name = st.sidebar.selectbox('Select Exchange', list(EXCHANGE_URLS.keys()))
+    num_pairs = st.sidebar.number_input('Number of Pairs', min_value=1, max_value=20, value=10)
 
-if st.sidebar.button('Get Pairs'):
-    if exchange_name.lower() == 'uniswap':
-        pairs = get_token_pairs_with_highest_liquidity('uniswap', num_pairs)
-    elif exchange_name.lower() == 'sushiswap':
-        pairs = get_token_pairs_with_highest_liquidity('sushiswap', num_pairs)
-    else:
-        st.error('Invalid exchange name.')
+    if st.sidebar.button('Get Pairs'):
+        try:
+            pairs = get_token_pairs_with_highest_liquidity(exchange_name, num_pairs)
+        except requests.exceptions.RequestException as e:
+            st.error(f"Request failed: {e}")
+            return
 
-    if pairs:
-        st.subheader(f'{exchange_name} token pairs with highest liquidity:')
-        for pair in pairs:
-            st.write(format_pair(pair))
-    else:
-        st.warning('No token pairs found.')
+        if pairs:
+            st.subheader(f'{exchange_name} token pairs with highest liquidity:')
+            for pair in pairs:
+                st.write(format_pair(pair))
+        else:
+            st.warning('No token pairs found.')
+
+
+if __name__ == "__main__":
+    main()
